7154f025a164971653efdef466c5966b6e4bb3d5
Daniel <83504117+ELODollZ@users.noreply.github.com>
2024-05-10 14:18:55 +0200

    Delete credentials.py

--- credentials.py
+++
@@ -1,9 +0,0 @@
-credentials = {
-    'ssid' : 'name',
-    'password' : 'pass',
-    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'name',
-    'ADAFRUIT_IO_KEY' : b'key',
-    'ADAFRUIT_IO_FEEDNAME' : b'feed'
-    }
-
